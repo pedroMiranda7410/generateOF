61fe8cd48dc19fc34d91bf40db48610178418d2c
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2022-06-14 10:12:46 -0300

    Modularização do Generate

--- config/checkPersonalFiles.js
+++ config/checkPersonalFiles.js
@@ -46,51 +46,54 @@ module.exports.checkPersonalFiles = async () => {
     var hasUserJson = false;
 
     try {
+
         const userConfig = require('../config/userConfig.json');
-        const defaultFiles = require('./defaultFiles');
+        const defaultFiles = require('./defaultFiles.js');
+
         var directoryOF = userConfig.directoryOF;
+
+        var cmd = `cd ${directoryOF} && ls`;
+        var result = await system.execShellCommand(cmd);
+        var files = result.split("\n");
+
+        var cmdConfig = `cd ${directoryOF}/config && ls`;
+        var resultConfig = await system.execShellCommand(cmdConfig);
+        var filesConfig = resultConfig.split("\n");
+
+        files.forEach(file => {
+            if (file.includes("user.json"))
+                hasUserJson = true;
+        });
+
+        filesConfig.forEach(file => {
+            if (file.includes("passwords.json"))
+                hasPassword = true;
+        });
+
+        if (!hasPassword) {
+
+            var filePath = `${directoryOF}/config/passwords.json`;
+
+            fs.writeFile(filePath, defaultFiles.defaultPasswords, function (err) {
+                if (err) { return console.log(err); }
+            });
+
+        }
+
+        if (!hasUserJson) {
+
+            var filePath = `${directoryOF}/user.json`;
+
+            fs.writeFile(filePath, defaultFiles.defaultUserJson, function (err) {
+                if (err) { return console.log(err); }
+            });
+
+        }
+
+        return true;
+
     } catch (error) {
         return false;
     }
-
-    var cmd = `cd ${directoryOF} && ls`;
-    var result = await system.execShellCommand(cmd);
-    var files = result.split("\n");
-
-    var cmdConfig = `cd ${directoryOF}/config && ls`;
-    var resultConfig = await system.execShellCommand(cmdConfig);
-    var filesConfig = resultConfig.split("\n");
-
-    files.forEach(file => {
-        if (file.includes("user.json"))
-            hasUserJson = true;
-    });
-
-    filesConfig.forEach(file => {
-        if (file.includes("passwords.json"))
-            hasPassword = true;
-    });
-
-    if (!hasPassword) {
-
-        var filePath = `${directoryOF}/config/passwords.json`;
-
-        fs.writeFile(filePath, defaultFiles.defaultPasswords, function (err) {
-            if (err) { return console.log(err); }
-        });
-
-    }
-
-    if (!hasUserJson) {
-
-        var filePath = `${directoryOF}/user.json`;
-
-        fs.writeFile(filePath, defaultFiles.defaultUserJson, function (err) {
-            if (err) { return console.log(err); }
-        });
-
-    }
-
-    return true;
 
 }
