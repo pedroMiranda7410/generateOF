8f3a0103941f3251e4aa9f769a52a3e283551d4f
C1321100 Wesley Matheus Vauthier <c1321100@interno.bb.com.br>
2022-09-08 20:29:00 -0300

    Ajustes de Detecção de Arquivos

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 // Instalação do Fortune
-sudo apt-get install -y fortune-mod fortunes fortunes-br
+//sudo apt-get install -y fortune-mod fortunes fortunes-br
 
 //Indicação de medalhas pra saber quem tem mais pontos
 
@@ -8,3 +8,6 @@ sudo apt-get install -y fortune-mod fortunes fortunes-br
 
 //Analistas tem que usar o 4.4.1
 //Documentação tem que usar o 2.1.1 -> Lembrar de enviar via anexo no GENTI
+
+//Pra configurações de VDI tem que usar o 5.23.1
+//Pra passagem de conhecimento tem que usar o impl-software para integração - passagem
